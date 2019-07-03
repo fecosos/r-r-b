e0c364cdeedf639c60193550030deef4ec8f387f
Ruy Mallo <ruymallo@gmail.com>
2019-07-02 22:44:30 -0300

    Add thunk middleware

--- src/store/StoreProvider.js
+++ src/store/StoreProvider.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from './reducers';
 
 const isEnvDev = process.env.NODE_ENV === 'development';
 
 const middlewares = isEnvDev
-  ? [logger]
-  : [];
+  ? [thunk, logger]
+  : [thunk];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
--- src/store/actions/testAction.js
+++ src/store/actions/testAction.js
@@ -1,8 +1,10 @@
 import { TEST_CONSTANT } from '../constants';
 
 export function testAction(value) {
-  return {
-    type: TEST_CONSTANT,
-    value
+  return dispatch => {
+    dispatch({
+      type: TEST_CONSTANT,
+      value
+    });
   };
 }
